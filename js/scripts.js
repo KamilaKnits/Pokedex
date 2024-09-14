c9728875d9f52d5c547059bb4430dffa752db2cb
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2024-09-14 19:47:15 -0400

    Update scripts.js
    created a list of pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-let favoriteFood = 'noodles';
-document.write(favoriteFood);
+pokemonList = [
+    {
+        name: 'Bellsprout',
+        height: '.7',
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'chansey',
+        height: '1.1',
+        type: 'normal'
+    }, {
+        name: 'Sudowoodo',
+        height: '1.2',
+        type: 'rock'
+    }, {
+        name: 'Mareep',
+        height: '.6',
+        type: 'electric'
+    }
 
-alert('Hello World');
+];
+
+// pokedex.org as reference, create at least 3 objects in your PokemonLst
