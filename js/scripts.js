cadaf3ca27fd2f7197f6b682131ecd2d3329340d
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2024-09-28 22:29:03 -0400

    Update scripts.js
    refactored code to inlcude IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,49 @@
-let pokemonList = [
-    {
-        name: 'Bellsprout',
-        height: '.7',
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Chansey',
-        height: '1.1',
-        type: ['normal']
-    }, {
-        name: 'Sudowoodo',
-        height: '1.2',
-        type: ['rock']
-    }, {
-        name: 'Mareep',
-        height: '.6',
-        type: ['electric']
+let pokemonRepository = (function () {
+  
+    let pokemonList = [
+      {
+          name: 'Bellsprout',
+          height: '.7',
+          type: ['grass', 'poison']
+      },
+      {
+          name: 'Chansey',
+          height: '1.1',
+          type: ['normal']
+      }, 
+      {
+          name: 'Sudowoodo',
+          height: '1.2',
+          type: ['rock']
+      }, 
+      {
+          name: 'Mareep',
+          height: '.6',
+          type: ['electric']
+      }
+  
+  ];
+    
+    function add(pokemon){
+      pokemonList.push(pokemon);
     }
-
-];
-
-
-
-for (let i=0; i < pokemonList.length; i++){
-    let pokemon = pokemonList[i];
-    document.write("<p>" + pokemon.name + "  ( height: " + " " + pokemon.height + ")" +"</p>" );
-    if (pokemon.height < .7){
-      document.write(" - Wow that's tiny!");
-    }
-  };
-
+    
+    function getAll(){
+       return pokemonList;
+     }
+   
+    return {
+      add: add,
+      getAll: getAll
+      
+    };
+  })();
+  
+  let pokemons = pokemonRepository.getAll();
+  pokemons.forEach( pokemon => {
+   document.write("<p>" + pokemon.name + "  ( height: " + " " + pokemon.height + ")" +"</p>" );
+    })
+  pokemonRepository.add({name: 'Piplup',height: '1.04',type: ['water']});
+  console.log(pokemonRepository.getAll());
+  
+  
