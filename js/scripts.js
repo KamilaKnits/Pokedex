7ff0e20e158bc79480d6e48a5515da8cf357ae29
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2024-09-29 23:08:53 -0400

    Update scripts.js
    added event listener and cleaned up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,72 @@
 let pokemonRepository = (function () {
-  
+
     let pokemonList = [
-      {
-          name: 'Bellsprout',
-          height: '.7',
-          type: ['grass', 'poison']
-      },
-      {
-          name: 'Chansey',
-          height: '1.1',
-          type: ['normal']
-      }, 
-      {
-          name: 'Sudowoodo',
-          height: '1.2',
-          type: ['rock']
-      }, 
-      {
-          name: 'Mareep',
-          height: '.6',
-          type: ['electric']
-      }
-  
-  ];
-    
-    function add(pokemon){
-      pokemonList.push(pokemon);
+        {
+            name: 'Bellsprout',
+            height: '.7',
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Chansey',
+            height: '1.1',
+            type: ['normal']
+        },
+        {
+            name: 'Sudowoodo',
+            height: '1.2',
+            type: ['rock']
+        },
+        {
+            name: 'Mareep',
+            height: '.6',
+            type: ['electric']
+        }
+
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-    
-    function getAll(){
-       return pokemonList;
-     }
-   
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listOfPokemons = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        button.addEventListener("click", function () {
+            showDetails(pokemon);
+        });
+        listOfPokemons.appendChild(button);
+        pokemonList.appendChild(listOfPokemons);
+    }
+
+
+
     return {
-      add: add,
-      getAll: getAll
-      
+        add: add,
+        getAll: getAll,
+        showDetails: showDetails,
+        addListItem: addListItem
+
+
     };
-  })();
-  
-  let pokemons = pokemonRepository.getAll();
-  pokemons.forEach( pokemon => {
-   document.write("<p>" + pokemon.name + "  ( height: " + " " + pokemon.height + ")" +"</p>" );
-    })
-  pokemonRepository.add({name: 'Piplup',height: '1.04',type: ['water']});
-  console.log(pokemonRepository.getAll());
-  
-  
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Piplup', height: '1.04', type: ['water'] });
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
+
