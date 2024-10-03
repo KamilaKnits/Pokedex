d968284e38e5455534afa3ccf03038946a96998d
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2024-10-02 20:41:49 -0400

    Update scripts.js
    added API to pull pokemons from

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,7 @@
 let pokemonRepository = (function () {
 
-    let pokemonList = [
-        {
-            name: 'Bellsprout',
-            height: '.7',
-            type: ['grass', 'poison']
-        },
-        {
-            name: 'Chansey',
-            height: '1.1',
-            type: ['normal']
-        },
-        {
-            name: 'Sudowoodo',
-            height: '1.2',
-            type: ['rock']
-        },
-        {
-            name: 'Mareep',
-            height: '.6',
-            type: ['electric']
-        }
-
-    ];
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
         pokemonList.push(pokemon);
@@ -30,11 +9,7 @@ let pokemonRepository = (function () {
 
     function getAll() {
         return pokemonList;
-    }
-
-    function showDetails(pokemon) {
-        console.log(pokemon);
-    }
+    } 
 
     function addListItem(pokemon) {
         let pokemonList = document.querySelector(".pokemon-list");
@@ -42,31 +17,66 @@ let pokemonRepository = (function () {
         let button = document.createElement("button");
         button.innerText = pokemon.name;
         button.classList.add("button-class");
-        button.addEventListener("click", function () {
-            showDetails(pokemon.name);
-        });
         listOfPokemons.appendChild(button);
         pokemonList.appendChild(listOfPokemons);
+      button.addEventListener("click", function(event){
+        showDetails(pokemon);
+      });
     }
 
+    function loadList(){
+        return fetch(apiUrl).then(function (response){
+            return response.json();
+        }).then(function (json){
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+              console.log(pokemon);
+            });
+        }).catch(function (e){
+            console.error(e);
+        })
+    }
+  
 
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(item) {
+  pokemonRepository.loadDetails(item).then(function () 
+   {
+    console.log(item);
+  });
+  }
 
     return {
         add: add,
         getAll: getAll,
-        showDetails: showDetails,
-        addListItem: addListItem
-
-
+      addListItem: addListItem,
+        loadList: loadList,
+      loadDetails:loadDetails,
+      showDetails: showDetails
     };
 })();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Piplup', height: '1.04', type: ['water'] });
 
-console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
 });
-
